b0611e5cb8039e6ef47f7adba61fb6fcab7e1756
DiaaJamel <diaa_jamel@yahoo.com>
2021-03-07 20:41:29 +0200

    ITBeep-Task update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,29 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
   <link rel="stylesheet" href="{{asset('css/style.css')}}">
-  <title>Document</title>
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <title>ITBeep Task</title>
 </head>
 <body>
 <header>
 </header>
-
+<nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">ITBeep</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
 <main class="container mt-5">
     @yield("content")
 </main>
